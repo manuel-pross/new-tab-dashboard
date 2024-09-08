2b39585eced2f8b3a01edb1f59ee4b36eef305e6
Manuel@Arch <pro_manuel@yahoo.de>
2024-09-08 10:25:21 +0200

    feat: add clock component

--- src/components/ArchLogo/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function ArchLogo() {
-  return (
-    <img
-      src="./public/arch_linux_logo_transparency.png"
-      alt="Logo of the arch linux distribution"
-      className="w-[200px] h-auto"
-    ></img>
-  );
-}
