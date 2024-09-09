c6996044ac2d5130f0200ed553c296d5722c1ba4
Manuel@Arch <pro_manuel@yahoo.de>
2024-09-09 17:06:14 +0200

    fix: remove currently unused state

--- src/components/Bookmarks/intex.tsx
+++ src/components/Bookmarks/intex.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 
 export default function Bookmarks() {
-  const [bookmarks, setBookmarks] = useState([]);
+  //const [bookmarks, setBookmarks] = useState([]);
 
   useEffect(() => {
     // Fetch all bookmarks
     chrome.bookmarks.getTree((bookmarkTreeNodes) => {
       console.log("Bookmarks:", bookmarkTreeNodes);
-      setBookmarks(bookmarkTreeNodes);
+      //setBookmarks(bookmarkTreeNodes);
     });
   });
   return <p>hello from bookmarks</p>;
