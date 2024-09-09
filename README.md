e925af586cbfb82be1bfc9770ad9759161f116b8
Manuel@Arch <pro_manuel@yahoo.de>
2024-09-09 16:37:08 +0200

    feat: change setup to older vite to use crxjs for easier access on chrome api

--- README.md
+++
@@ -1,1 +0,0 @@
-# Chrome extension for custom startpage
---
+++ src/background.ts
@@ -0,0 +1,8 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed.");
+
+  // Fetch all bookmarks
+  chrome.bookmarks.getTree((bookmarkTreeNodes) => {
+    console.log("Bookmarks:", bookmarkTreeNodes);
+  });
+});
