a33fa8b49d73d941d03497e4d753a644b9f839d1
Manuel@Arch <pro_manuel@yahoo.de>
2024-09-10 11:09:48 +0200

    fix: prevent inserting character: b after opening searchBar with shortcut: b

--- src/components/BookmarksSearch/index.tsx
+++ src/components/BookmarksSearch/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Bookmark, ChromeBookmarkResponse } from "../../types";
 import {
   checkForActiveSelectionField,
@@ -9,7 +9,8 @@ export default function BookmarksSearch() {
   const searchRef = useRef<HTMLInputElement>(null);
 
   const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
-  const [isSearchOpened, setIsSearchOpened] = useState<boolean>(false);
+  const [isSearchOpened, setIsSearchOpened] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     chrome.bookmarks.getTree((bookmarkTreeNodes) => {
@@ -43,7 +44,9 @@ export default function BookmarksSearch() {
 
     if (!isBKey || isSearchOpened || checkForActiveSelectionField()) return;
 
+    event.preventDefault();
     setIsSearchOpened(true);
+    setSearchTerm("");
   };
 
   const handleMouseDown = (event: MouseEvent) => {
@@ -59,12 +62,20 @@ export default function BookmarksSearch() {
     setIsSearchOpened(false);
   };
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div>
-      {isSearchOpened ? (
-        <input ref={searchRef} onKeyDown={handleEscape} type="text"></input>
-      ) : (
-        <div>No shortcut</div>
+      {isSearchOpened && (
+        <input
+          ref={searchRef}
+          value={searchTerm}
+          onKeyDown={handleEscape}
+          onChange={handleChange}
+          type="text"
+        ></input>
       )}
     </div>
   );
