4525525e44aae918d577b82300f5a99f0152c578
Manuel@Arch <pro_manuel@yahoo.de>
2024-09-11 17:47:15 +0200

    feat: add fira code as default font

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,7 @@ import CustomDate from '../CustomDate';
 
 export default function Layout() {
   return (
-    <div className="flex justify-center w-screen h-screen bg-tokyo-storm">
+    <div className="flex justify-center w-screen h-screen bg-tokyo-storm font-fira">
       <div className="flex justify-between pt-6 px-8 w-full max-w-[1400px]">
         <Clock />
         <CustomDate />
