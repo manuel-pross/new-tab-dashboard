23e504cb3a74b4ef53ac7fa1028ba288d26ee0c7
Manuel@Arch <pro_manuel@yahoo.de>
2024-09-11 11:37:26 +0200

    build: add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), eslint(), crx({ manifest })],
 });
