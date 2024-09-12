dfb3ad92f6f6072b1ebe1119eb3c53b10dfec4e8
Manuel@Arch <pro_manuel@yahoo.de>
2024-09-12 09:04:57 +0200

    fix: remove eslint due to breaking npm run build

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json';
-import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), crx({ manifest })],
+  plugins: [react(), crx({ manifest })],
 });
