7ee55bca0e9496fe2a36090398e749022afc852c
Manuel Proß <pro_manuel@yahoo.de>
2024-10-20 17:22:30 +0200

    feat: add react query, add api call for random pokemon, add card flip effect

--- src/components/RandomPokemon/index.tsx
+++ src/components/RandomPokemon/index.tsx
@@ -1,3 +1,53 @@
+import { useQuery } from 'react-query';
+import { Pokemon } from '../../types';
+import { getRandomNumber } from '../../utils';
+
 export default function RandomPokemon() {
-  return <div className="">hello</div>;
+  const {
+    data: pokemon,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['pokemon'],
+    queryFn: async () => {
+      const randomNumber = getRandomNumber(1, 1000);
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${randomNumber}`
+      );
+      const pokemon = (await res.json()) as Pokemon;
+      return pokemon;
+    },
+  });
+
+  if (isLoading) {
+    return <p>is loading</p>;
+  }
+
+  if (error) {
+    return <p>error while fetching</p>;
+  }
+
+  if (!pokemon) {
+    return <p>no pokemon</p>;
+  }
+
+  return (
+    <div className="card">
+      <div className="card-content">
+        <div className="card-front">
+          <img
+            width="120px"
+            src={pokemon.sprites.front_default}
+            alt={`the sprite of ${pokemon.name}`}
+          ></img>
+        </div>
+        <div className="card-back">
+          <p>{pokemon.name}</p>
+          {pokemon.types.map((type) => {
+            return <p>{type.type.name}</p>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -58,3 +58,11 @@ export function focusFirstSearchResult() {
 
   firstResult?.focus();
 }
+
+export function getRandomNumber(min: number, max: number): number | null {
+  if (min > max) {
+    return null;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
